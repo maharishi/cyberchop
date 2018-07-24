3040452d138932efff1a602af9cd735df0e90573
Maharishi Bhatia <maharishi@users.noreply.github.com>
2018-07-24 15:42:49 -0400

    Small fixes to the UI to get the upsert done.

--- html/cgi-bin/cyberchopcontroller.py
+++ html/cgi-bin/cyberchopcontroller.py
@@ -6,8 +6,7 @@ def getDatabase():
     conn = sqlite3.connect('status.db')
     return conn
 
-def getConnection():
-    conn = getDatabase()
+def getCursor(conn):
     conn.row_factory = sqlite3.Row
     return conn.cursor()
 
@@ -45,22 +44,40 @@ def disable(form):
 
 def friendly(form):
     friendlynameitems=[]
-    c = getDatabase()
+    conn = getDatabase()
     rowid=form.getfirst("rowid", None)
     name=form.getfirst("name", None)
     if(rowid is not None and name is not None):
-        sql = ''' INSERT INTO machine_details (mac_address, friendly_name) SELECT mac_address, ? FROM machine_list WHERE ip_address = ? OR rowid = ? ;'''
-        with c:
-            rename=(name, rowid, rowid)
-            c.execute(sql, rename)
-    conn = getConnection()
-    for row in conn.execute("SELECT ml.rowid, ml.ip_address, md.friendly_name FROM machine_list AS ml LEFT  OUTER JOIN machine_details AS md ON md.mac_address = ml.mac_address"):
+        selectsql = '''
+            SELECT ml.mac_address, ml.ip_address, md.friendly_name 
+            FROM machine_list AS ml LEFT OUTER JOIN machine_details AS md 
+                ON md.mac_address = ml.mac_address
+            WHERE ml.rowid = ? OR ml.ip_address = ?;
+        '''
+        for row in getCursor(conn).execute(selectsql, (rowid, rowid)):
+            if(row['friendly_name'] is None):
+                insertsql = ''' 
+                    INSERT INTO machine_details (mac_address, friendly_name) 
+                    VALUES (?, ?);
+                '''
+                with conn:
+                    conn.execute(insertsql, (row['mac_address'], name))
+            else:
+                updatesql = ''' 
+                    UPDATE machine_details 
+                    SET friendly_name = ? 
+                    WHERE mac_address = ?;
+                '''
+                with conn:
+                    conn.execute(updatesql, (name, row['mac_address']))
+    for row in getCursor(conn).execute("SELECT ml.rowid, ml.ip_address, md.friendly_name FROM machine_list AS ml LEFT  OUTER JOIN machine_details AS md ON md.mac_address = ml.mac_address"):
         friendlynameitems.append({'rowid': row['rowid'], 'ip_address': row['ip_address'], 'friendly_name': row['friendly_name']})
+    conn.close()
     response={ "friendlist": friendlynameitems, "error":"" }
     print(json.JSONEncoder().encode(response))
 
 def default(form, log=""):
-    c = getConnection()
+    c = getCursor(getDatabase())
     disableditems=[]
     enableditems=[]
     for row in c.execute('SELECT rowid, ip_address FROM machine_list where active = 0'):
